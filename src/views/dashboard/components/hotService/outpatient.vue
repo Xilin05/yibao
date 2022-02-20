122d6e3ec0a134024b4e36b2232cc39dea3b452d
Xilin05 <273332529@qq.com>
2022-02-21 01:59:29 +0800

    完善门诊预约界面及基础逻辑功能

--- src/views/dashboard/components/hotService/outpatient.vue
+++ src/views/dashboard/components/hotService/outpatient.vue
@@ -1,5 +1,98 @@
 <template>
-  <div></div>
+  <div class="map-container">
+    <van-nav-bar
+      title="门诊预约"
+      left-text="返回"
+      left-arrow
+      fixed
+      @click-left="onClickLeft"
+      :placeholder="true"
+    />
+    <van-empty
+      v-if="position.length == 0"
+      image="error"
+      description="未找到相关位置"
+    />
+    <div class="main-cards" v-if="position.length != 0">
+      <van-divider :style="{ borderColor: '#e3e3e3', padding: '0 22px' }"
+        >请选择医疗机构进行门诊预约</van-divider
+      >
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="getNextPage()"
+      >
+        <div
+          class="position-card"
+          v-for="(item, i) in position"
+          :key="i"
+          @click="toDetail(item)"
+        >
+          <h2>{{ i + 1 }}. {{ item.title }}</h2>
+          <div>
+            <span>地址：</span>
+            <span>
+              {{ item.address }}
+            </span>
+          </div>
+          <!-- <div class="otherInfo"> -->
+          <div>
+            <span>电话：</span>
+            <span v-if="item.tel">
+              {{ item.tel }}
+            </span>
+            <span v-if="!item.tel">暂无</span>
+          </div>
+          <div>
+            <span>距离：</span>
+            <span> {{ item._distance | filteDestinate }}km </span>
+          </div>
+          <!-- </div> -->
+        </div>
+      </van-list>
+    </div>
+    <van-dialog
+      v-model="show"
+      title="防疫期间门诊就诊提示"
+      confirm-button-text="我知道了"
+      confirm-button-color="rgb(87, 135, 255)"
+      show-cancel-button
+      @confirm="toAppointment()"
+    >
+      <div class="tips">
+        <div>
+          1.
+          近30天内如取消预约并线上退号累计达到3次的，该诊疗卡将在本轮30天期限内暂停线上预约挂号功能（从本轮周期第一次退号日期起计算30天）
+        </div>
+        <div>2. 健康通行登记绿码，测体温正常，方可进入医院</div>
+        <div>
+          3.
+          医院实行人流管控，请按预约时段提前半小时来到医院。为减少医院入口因申请穗康码导致的拥堵，请务必来院就诊前提前申请穗康码，以便快速通行。
+        </div>
+        <div>
+          4.
+          门诊患者及陪同人员（限1名）健康通行登记绿码，测体温正常，方可进入医院。
+        </div>
+        <div>
+          5.有以下情况之一的，请不要预约挂号：
+          <br />
+          （1）健康码为红码、黄码人员；
+          <br />
+          （2）21天内有境外或国内中高风险地区旅居史；
+          <br />
+          （3）新冠病毒肺炎确诊或疑似病例接触史人员；
+          <br />
+          （4）有发热（体温≥37.3℃）或嗅觉味觉减退或急性咳嗽或乏力等流感样症状者。
+        </div>
+        <div>
+          6. 所有来院人员（含病人及陪同人员）凭身份证或医保卡进入医院。
+          <br />
+          <span style="color: red">[注] 现场支付，线上预约不收费。</span>
+        </div>
+      </div>
+    </van-dialog>
+  </div>
 </template>
 
 <script>
@@ -8,22 +101,278 @@
 
 export default {
   //import引入的组件需要注入到对象中才能使用
+  filters: {
+    filteDestinate: function (dictinc) {
+      let dic = dictinc / 1000;
+      return dic.toFixed(2);
+    },
+  },
   components: {},
   props: {},
   data() {
     //这里存放数据
-    return {};
+    return {
+      position: [],
+      loading: false,
+      finished: false,
+      location: {},
+      show: false,
+      acidAddress: {},
+      routerItem: {},
+      index: 1,
+    };
   },
   //计算属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    onClickLeft() {
+      this.$router.go(-1);
+    },
+    async initCity() {
+      this.$toast.loading({
+        message: "正在定位...",
+        forbidClick: true,
+        duration: 0,
+        loadingType: "spinner",
+      });
+      var data = {
+        // key: 腾讯位置控制台申请的密钥
+        key: "KIPBZ-CBQ64-AF6UN-DXH6C-G7TAO-AXFX4",
+      };
+      // 腾讯位置根据ip信息获取地理位置的接口
+      var url = "https://apis.map.qq.com/ws/location/v1/ip";
+      data.output = "jsonp";
+      let res = await this.$jsonp(url, data);
+      console.log("初始化位置", res);
+      if (res.status == 0) {
+        if (res.count <= 20) {
+          this.finished = true;
+        }
+        if (res.result.ad_info.district == "") {
+          this.district = res.result.ad_info.city;
+          this.location = res.result.location;
+          this.searchOrganize(res.result.ad_info.city, res.result.location);
+        } else {
+          this.district = res.result.ad_info.district;
+          this.location = res.result.location;
+          this.searchOrganize(res.result.ad_info.district, res.result.location);
+        }
+        this.$toast.clear();
+      } else {
+        this.$toast({
+          message: "定位失败",
+          forbidClick: true,
+          duration: 2000,
+          loadingType: "spinner",
+          icon: "cross",
+        });
+      }
+    },
+    async searchOrganize(district, params) {
+      // let nearby = "nearby(" + params.lat + "," + params.lng + "," + "1000,1)";
+      let nearby =
+        "region(" + district + ",2," + params.lat + "," + params.lng + ")";
+      let category = "category=医疗保健";
+      var data = {
+        boundary: nearby,
+        page_size: 20,
+        page_index: this.index,
+        keyword: "医院",
+        filter: category,
+        orderby: "_distance",
+        // key: 腾讯位置控制台申请的密钥
+        key: "KIPBZ-CBQ64-AF6UN-DXH6C-G7TAO-AXFX4",
+      };
+      // 腾讯位置根据ip信息获取地理位置的接口
+      var url = "https://apis.map.qq.com/ws/place/v1/search";
+      data.output = "jsonp";
+      let res = await this.$jsonp(url, data);
+      console.log("获取机构", res);
+      if (res.status == 0) {
+        this.$toast.clear();
+        this.position = res.data;
+        if (res.count <= 20) {
+          this.finished = true;
+        }
+      } else {
+        this.$toast({
+          message: "定位失败",
+          forbidClick: true,
+          duration: 2000,
+          loadingType: "spinner",
+          icon: "cross",
+        });
+      }
+    },
+    async getNextPage() {
+      if (!this.location.lat || !this.location.lng) {
+        this.loading = false;
+        return;
+      }
+      this.index += 1;
+      console.log(this.index);
+      let nearby =
+        "region(" +
+        this.district +
+        ",2," +
+        this.location.lat +
+        "," +
+        this.location.lng +
+        ")";
+      let category = "category=医疗保健";
+      var data = {
+        boundary: nearby,
+        page_size: 20,
+        page_index: this.index,
+        keyword: "医院",
+        filter: category,
+        orderby: "_distance",
+        // key: 腾讯位置控制台申请的密钥
+        key: "KIPBZ-CBQ64-AF6UN-DXH6C-G7TAO-AXFX4",
+      };
+      // 腾讯位置根据ip信息获取地理位置的接口
+      var url = "https://apis.map.qq.com/ws/place/v1/search";
+      data.output = "jsonp";
+      let res = await this.$jsonp(url, data);
+      console.log("获取下一页", res);
+      if (res.status == 0) {
+        this.position.push(...res.data);
+        if (res.data.length < 20) {
+          this.finished = true;
+        }
+      } else {
+        // this.getNextPage();
+        this.$toast({
+          message: "获取失败",
+          forbidClick: true,
+          duration: 2000,
+          loadingType: "spinner",
+          icon: "cross",
+        });
+      }
+      this.loading = false;
+    },
+    toDetail(item) {
+      this.routerItem = item;
+      this.show = true;
+      // this.$router.push({
+      //   name: "AcidAppointment",
+      //   params: item,
+      // });
+    },
+    toAppointment() {
+      this.$router.push({
+        name: "PatientAppointment",
+        params: this.routerItem,
+      });
+    },
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
-  created() {},
+  created() {
+    this.initCity();
+  },
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {},
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.map-container {
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background-color: #f9fbfd;
+  .main-cards {
+    width: 100%;
+    .position-card {
+      width: 80%;
+      height: auto;
+      padding: 10px 16px;
+      border-radius: 0.5rem;
+      margin: 0.8rem auto 0rem auto;
+      background-color: #ffffff;
+      box-shadow: 0px 0px 8px 0px #ebebeb;
+      h2 {
+        margin: 0.2rem;
+        font-size: 1rem;
+        color: rgb(48, 48, 48);
+      }
+      div {
+        width: 100%;
+        font-size: 0.9rem;
+        color: rgb(155, 155, 155);
+        display: flex;
+        justify-content: space-between;
+        span:nth-child(1) {
+          line-height: 1.3rem;
+          width: 15%;
+          height: auto;
+        }
+        span:nth-child(2) {
+          line-height: 1.3rem;
+          height: auto;
+          width: 84%;
+        }
+      }
+    }
+    .tips {
+      margin-top: 1rem;
+      font-size: 0.8rem;
+      color: #b6b6b6;
+      text-align: center;
+    }
+  }
+
+  /deep/ .van-divider {
+    margin: 8px 0;
+  }
+  /deep/ .van-step__title {
+    width: 95%;
+    display: flex;
+    margin: 0 auto;
+    align-items: center;
+    padding: 10px 0px;
+    .title {
+      // background-color: rgb(206, 206, 206);
+      font-size: 0.9rem;
+      font-weight: 600;
+      width: 70%;
+      // color: black;
+    }
+    .record {
+      width: 30%;
+      font-size: 0.7rem;
+      font-weight: 400;
+      color: #9e9e9e;
+      display: flex;
+      flex-wrap: wrap;
+      text-align: right;
+      span {
+        width: 100%;
+      }
+    }
+  }
+  /deep/ .van-dialog__content {
+    width: 90%;
+    margin: 0 auto;
+  }
+  // /deep/ .van-step--vertical:not(:last-child)::after {
+  //   border: none;
+  // }
+  .tips {
+    // font-size: 0.9rem;
+    h3 {
+      font-size: 0.9rem;
+    }
+    div {
+      margin: 0.5rem auto;
+      width: 100%;
+      // padding: 0px 15px;
+      font-size: 0.9rem;
+      // text-indent: 1rem;
+    }
+  }
+}
+</style>
--- src/views/dashboard/components/hotService/patientDetail.vue
+++ src/views/dashboard/components/hotService/patientDetail.vue
@@ -1,29 +1,329 @@
 <template>
-  <div></div>
+  <div class="aciddetail-container">
+    <van-nav-bar
+      title="预约信息提交"
+      left-text="返回"
+      left-arrow
+      fixed
+      @click-left="onClickLeft"
+      :placeholder="true"
+    />
+    <div class="identi">
+      <van-cell-group>
+        <van-field
+          v-model="form.name"
+          label="受诊人"
+          placeholder="请输入受诊人员"
+        />
+        <van-field
+          v-model="form.phoneNumber"
+          label="电话号码"
+          placeholder="请输入电话号码"
+        />
+        <van-field
+          v-model="form.addressName"
+          label="预约机构"
+          placeholder="请选择预约医院/机构"
+          readonly
+        />
+        <van-field
+          readonly
+          clickable
+          :value="form.department"
+          label="预约科室"
+          name="departmentPicker"
+          placeholder="点击选择预约科室"
+          @click="showPicker = true"
+        />
+        <van-popup round v-model="showPicker" position="bottom">
+          <van-picker
+            show-toolbar
+            :columns="department"
+            @confirm="onConfirm"
+            @cancel="showPicker = false"
+          />
+        </van-popup>
+      </van-cell-group>
+      <div></div>
+    </div>
+    <div class="forminfo"></div>
+    <div class="formtime">
+      <div class="days">
+        <div v-for="(item, i) in days" :key="i">
+          {{ item.weekday }}
+          <div
+            :class="{
+              day_Actived: dayActived == i,
+            }"
+            class="day"
+            @click="selectDay(i, item)"
+          >
+            {{ item.day }}
+          </div>
+        </div>
+      </div>
+      <div class="times">
+        <div
+          :class="{
+            timeDisabled: item.resources == 0,
+            timeActived: weekdayActiced == j && item.resources != 0,
+          }"
+          class="timeCard"
+          v-for="(item, j) in resources"
+          :key="j"
+          @click="selectWeekDay(j, item)"
+        >
+          <div class="time">{{ item.time }}</div>
+          <div class="number">
+            <van-tag
+              color="rgb(220, 230, 255)"
+              text-color="rgb(46, 106, 255)"
+              size="medium"
+              >号源：{{ item.resources }}</van-tag
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="tips">
+      <div>温馨提示：</div>
+      <div>1. 线上预约不收取任何费用，线下现场再进行支付。</div>
+      <div>2. 为了不占取资源，请预约适合自己的时间段，</div>
+      <div>3. 预约后若没有到达现场就诊将记录进入系统。</div>
+      <div>4. 成功提交预约后可以在个人中心页面查看预约记录。</div>
+    </div>
+    <van-submit-bar
+      :price="0"
+      button-text="提交预约"
+      button-color="rgb(87, 135, 255)"
+      @submit="onSubmit"
+    />
+  </div>
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-
+import { dayJson } from "@/api/timeSelect";
 export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
   props: {},
   data() {
     //这里存放数据
-    return {};
+    return {
+      form: {
+        name: "赖喜铃",
+        phoneNumber: "17876374525",
+        addressName: "",
+        department: "",
+        date: "",
+        time: "",
+        submitTime: "",
+      },
+      days: [],
+      weekday: [],
+      resources: [],
+      weekdayActiced: null,
+      dayActived: 0,
+      showPicker: false,
+      department: [
+        "中医科",
+        "产科",
+        "儿科",
+        "内科",
+        "口腔颌面科",
+        "外科",
+        "妇科",
+        "报告解读科",
+        "整形美容科",
+        "男科",
+        "皮肤性病科",
+        "眼科",
+        "精神心理科",
+        "耳鼻咽喉科",
+        "肿瘤及防治科",
+        "营养科",
+        "骨伤科",
+      ],
+    };
   },
   //计算属性 类似于data概念
   computed: {},
   //监控data中的数据变化
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    onClickLeft() {
+      // this.$toast.success("返回");
+      this.$router.go(-1);
+    },
+    initDate() {
+      this.resources = dayJson[0];
+      let days = [];
+      for (let i = 0; i <= 5; i++) {
+        let obj = {};
+        let test = new Date().getTime() + 86400000 * i;
+        let date = new Date(test);
+        obj.day = `${date.getMonth() + 1}/${date.getDate()}`;
+        obj.weekday = "周" + "日一二三四五六".charAt(date.getDay());
+        days.push(obj);
+      }
+      this.days = days;
+    },
+    formatDate(date) {
+      return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+    },
+    onConfirm(value) {
+      console.log(value);
+      this.form.department = value;
+      this.showPicker = false;
+    },
+    selectDay(key, item) {
+      this.dayActived = key;
+      this.resources = dayJson[key];
+    },
+    selectWeekDay(key, item) {
+      this.weekdayActiced = key;
+    },
+    onSubmit() {
+      this.$toast.success("提交预约成功！");
+      this.$router.push("/dashboard");
+    },
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
-  created() {},
+  created() {
+    this.initDate();
+  },
   //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
+  mounted() {
+    this.form.addressName = this.$route.params.title;
+  },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.aciddetail-container {
+  // color: ;
+  // background-image: linear-gradient(135deg, #abdcff 10%, #0396ff 100%);
+  // background-image: linear-gradient( 135deg, #CE9FFC 10%, #7367F0 100%);
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background-color: #f9fbfd;
+  .identi {
+    box-shadow: 0px 0px 8px 0px #ebebeb;
+  }
+  .forminfo {
+  }
+  .formtime {
+    width: 100%;
+    height: auto;
+    margin-top: 1rem;
+    padding: 8px 0px;
+    background-color: #fff;
+    box-shadow: 0px 0px 8px 0px #ebebeb;
+    .days {
+      width: 100%;
+      height: auto;
+      padding: 5px 0px;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #ebebeb;
+      div {
+        // width: $(100/6);
+        flex: 1;
+        height: 100%;
+        line-height: 1rem;
+        text-align: center;
+        font-size: 0.9rem;
+        color: gray;
+        border: 1px 0px 1px 1px solid #ebebeb;
+        .day {
+          padding: 5px;
+          border-radius: 50%;
+          height: 2rem;
+          width: 2rem;
+          line-height: 2rem;
+          margin: 0.3rem auto;
+        }
+      }
+      div:last-child() {
+        border: 1px 1px 1px 0px solid #ebebeb;
+      }
+    }
+    .times {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      flex-wrap: wrap;
+      margin-top: 1rem;
+      .timeCard {
+        position: relative;
+        z-index: 3;
+        overflow: hidden;
+        width: 25%;
+        height: auto;
+        padding: 10px 6px;
+        border-radius: 0.5rem;
+        border: 1px solid rgb(238, 238, 238);
+        // box-shadow: 0px 0px 8px 0px #ebebeb;
+        margin-bottom: 0.8rem;
+        text-align: center;
+        line-height: 1.5rem;
+        .time {
+          font-size: 0.9rem;
+        }
+        .number {
+          font-size: 0.7rem;
+        }
+      }
+    }
+  }
+  .tips {
+    margin-top: 1rem;
+    font-size: 00.8rem;
+    color: #a1a1a1;
+    padding: 0px 16px;
+    line-height: 1.3rem;
+  }
+  /deep/ .van-submit-bar {
+    box-shadow: 0px 0px 8px 0px #ebebeb;
+  }
+}
+.day_Actived {
+  color: white !important;
+  background-color: rgb(46, 106, 255);
+  // background-color: rgb(220, 230, 255);
+}
+.timeActived {
+  // position: relative;
+  border: 1px solid rgb(87, 135, 255) !important;
+  box-sizing: content-box;
+}
+.timeActived::before {
+  position: absolute;
+  display: block;
+  border-top: 28px solid rgb(87, 135, 255);
+  border-left: 28px solid transparent;
+  top: 0;
+  right: 0;
+  content: "";
+  z-index: 1;
+}
+.timeActived::after {
+  font: normal normal normal 14px/1 "vant-icon";
+  position: absolute;
+  display: block;
+  right: 2px;
+  top: 2px;
+  content: "\e728";
+  // content: "\e013";
+  // font-size: 10pt;
+  font-weight: normal;
+  z-index: 2;
+  color: #fff;
+}
+.timeDisabled {
+  filter: grayscale(1);
+}
+</style>
